e444a118079c9ec59d5c34c47089994eadfc26e9
BuildTools <arnukren@gmail.com>
2016-06-17 12:25:49 -0400

    Fixed UI visibility bugs.

--- android/src/net/pixelstatic/pixeleditor/AndroidLauncher.java
+++ android/src/net/pixelstatic/pixeleditor/AndroidLauncher.java
@@ -1,11 +1,10 @@
 package net.pixelstatic.pixeleditor;
 
-import net.pixelstatic.pixeleditor.PixelEditor;
 import net.pixelstatic.utils.AndroidKeyboard;
+import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.graphics.Rect;
-import android.os.Build;
-import android.os.Bundle;
+import android.os.*;
 import android.view.View;
 import android.view.View.OnLayoutChangeListener;
 
@@ -47,5 +46,42 @@ public class AndroidLauncher extends AndroidApplication{
 		});
 
 		initialize(new PixelEditor(), config);
+	}
+
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus){
+		super.onWindowFocusChanged(hasFocus);
+		// When the window loses focus (e.g. the action overflow is shown),
+		// cancel any pending hide action. When the window gains focus,
+		// hide the system UI.
+		if(hasFocus){
+			delayedHide(300);
+		}else{
+			mHideHandler.removeMessages(0);
+		}
+	}
+
+	@SuppressLint("NewApi")
+	private void hideSystemUI(){
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_IMMERSIVE);
+	}
+
+	@SuppressLint("NewApi")
+	private void showSystemUI(){
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+	}
+
+	@SuppressLint("HandlerLeak")
+	private final Handler mHideHandler = new Handler(){
+		@SuppressLint("HandlerLeak")
+		@Override
+		public void handleMessage(Message msg){
+			hideSystemUI();
+		}
+	};
+
+	private void delayedHide(int delayMillis){
+		mHideHandler.removeMessages(0);
+		mHideHandler.sendEmptyMessageDelayed(0, delayMillis);
 	}
 }
